9e6a5e68aa89ceeb179844c19925630f16c4b2a0
Sergio Francisco <spinheiro.lf@gmail.com>
2022-07-23 12:22:14 -0300

    trocando o status de token

--- src/middlewares/token.js
+++ src/middlewares/token.js
@@ -4,7 +4,7 @@ const httpStatus = require('../helpers/statusCode');
 const token = (req, res, next) => {
     const { authorization } = req.headers;
     if (!authorization) {
-        res.status(httpStatus.badRequest).json({ message: 'Token not found' });
+        res.status(httpStatus.unauthorized).json({ message: 'Token not found' });
       }
       try {
        const validToken = Jwt.validateToken(authorization);
